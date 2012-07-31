7df88223eef71b86cc2fea09f2dffa83c3705ba7
Chris Risner <github@chrisrisner.com>
2012-07-31 12:51:46 -0400

    update headers

--- source/src/com/msdpe/shortifierdemo/misc/Constants.java
+++ source/src/com/msdpe/shortifierdemo/misc/Constants.java
@@ -1,7 +1,22 @@
+// ---------------------------------------------------------------------------------- 
+// Microsoft Developer & Platform Evangelism 
+//  
+// Copyright (c) Microsoft Corporation. All rights reserved. 
+//  
+// THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,  
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES  
+// OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE. 
+// ---------------------------------------------------------------------------------- 
+// The example companies, organizations, products, domain names, 
+// e-mail addresses, logos, people, places, and events depicted 
+// herein are fictitious.  No association with any real company, 
+// organization, product, domain name, email address, logo, person, 
+// places, or events is intended or should be inferred. 
+// ---------------------------------------------------------------------------------- 
 package com.msdpe.shortifierdemo.misc;
 
 public class Constants {
-	public static final String kShortifierRootUrl = "http://urlshortener.azurewebsites.net/";
-	public static final String kGetAllUrl = "http://urlshortener.azurewebsites.net/api-getall";
-	public static final String kAddUrl = "http://urlshortener.azurewebsites.net/api-add";
+	public static final String kShortifierRootUrl = "http://<your-subdomain>.azurewebsites.net/";
+	public static final String kGetAllUrl = "http://<your-subdomain>.azurewebsites.net/api-getall";
+	public static final String kAddUrl = "http://<your-subdomain>.azurewebsites.net/api-add";
 }
